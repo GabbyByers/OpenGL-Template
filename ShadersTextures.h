5e62fecf8817850dc4ddd077520bff33ed6d1624
♡ gabby ♡ <81900622+GabrielBuckingham@users.noreply.github.com>
2023-11-11 16:09:19 -0800

    Removed Comments, Further Cleanup

--- ShadersTextures.h
+++ ShadersTextures.h
@@ -9,68 +9,46 @@
 
 class Shader {
 public:
-	// Reference ID of the Shader Program
 	GLuint ID;
 	
-	// Constructor that build the Shader Program from 2 different shaders
 	Shader(const char* vertexFile, const char* fragmentFile) {
-		// Read vertexFile and fragmentFile and store the strings
 		std::string vertexCode = get_file_contents(vertexFile);
 		std::string fragmentCode = get_file_contents(fragmentFile);
 
-		// Convert the shader source strings into character arrays
 		const char* vertexSource = vertexCode.c_str();
 		const char* fragmentSource = fragmentCode.c_str();
 
-		// Create Vertex Shader Object and get its reference
 		GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
-		// Attach Vertex Shader source to the Vertex Shader Object
 		glShaderSource(vertexShader, 1, &vertexSource, NULL);
-		// Compile the Vertex Shader into machine code
 		glCompileShader(vertexShader);
-		// Checks if Shader compiled succesfully
 		compileErrors(vertexShader, "VERTEX");
 
-		// Create Fragment Shader Object and get its reference
 		GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-		// Attach Fragment Shader source to the Fragment Shader Object
 		glShaderSource(fragmentShader, 1, &fragmentSource, NULL);
-		// Compile the Vertex Shader into machine code
 		glCompileShader(fragmentShader);
-		// Checks if Shader compiled succesfully
 		compileErrors(fragmentShader, "FRAGMENT");
 
-		// Create Shader Program Object and get its reference
 		ID = glCreateProgram();
-		// Attach the Vertex and Fragment Shaders to the Shader Program
 		glAttachShader(ID, vertexShader);
 		glAttachShader(ID, fragmentShader);
-		// Wrap-up/Link all the shaders together into the Shader Program
 		glLinkProgram(ID);
-		// Checks if Shaders linked succesfully
 		compileErrors(ID, "PROGRAM");
 
-		// Delete the now useless Vertex and Fragment Shader objects
 		glDeleteShader(vertexShader);
 		glDeleteShader(fragmentShader);
 	}
 
-	// Activates the Shader Program
 	void Activate() {
 		glUseProgram(ID);
 	}
 
-	// Deletes the Shader Program
 	void Delete() {
 		glDeleteProgram(ID);
 	}
 
 private:
-	// Checks if the different Shaders have compiled properly
 	void compileErrors(unsigned int shader, const char* type) {
-		// Stores status of compilation
 		GLint hasCompiled;
-		// Character array to store error message in
 		char infoLog[1024];
 		if (type != "PROGRAM") {
 			glGetShaderiv(shader, GL_COMPILE_STATUS, &hasCompiled);
@@ -88,7 +66,6 @@ private:
 		}
 	}
 
-	// Reads a text file and outputs a string with everything in the text file
 	std::string get_file_contents(const char* filename) {
 		std::ifstream in(filename, std::ios::binary);
 		if (in) {
@@ -110,52 +87,33 @@ public:
 	GLenum type;
 
 	Texture(const char* image, GLenum texType, GLenum slot, GLenum format, GLenum pixelType) {
-		// Assigns the type of the texture ot the texture object
 		type = texType;
 
-		// Stores the width, height, and the number of color channels of the image
 		int widthImg, heightImg, numColCh;
-		// Flips the image so it appears right side up
 		stbi_set_flip_vertically_on_load(true);
-		// Reads the image from a file and stores it in bytes
 		unsigned char* bytes = stbi_load(image, &widthImg, &heightImg, &numColCh, 0);
 
-		// Generates an OpenGL texture object
 		glGenTextures(1, &ID);
-		// Assigns the texture to a Texture Unit
 		glActiveTexture(slot);
 		glBindTexture(texType, ID);
 
-		// Configures the type of algorithm that is used to make the image smaller or bigger
 		glTexParameteri(texType, GL_TEXTURE_MIN_FILTER, GL_NEAREST_MIPMAP_LINEAR);
 		glTexParameteri(texType, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 
-		// Configures the way the texture repeats (if it does at all)
 		glTexParameteri(texType, GL_TEXTURE_WRAP_S, GL_REPEAT);
 		glTexParameteri(texType, GL_TEXTURE_WRAP_T, GL_REPEAT);
 
-		// Extra lines in case you choose to use GL_CLAMP_TO_BORDER
-		// float flatColor[] = {1.0f, 1.0f, 1.0f, 1.0f};
-		// glTexParameterfv(GL_TEXTURE_2D, GL_TEXTURE_BORDER_COLOR, flatColor);
-
-		// Assigns the image to the OpenGL Texture object
 		glTexImage2D(texType, 0, GL_RGBA, widthImg, heightImg, 0, format, pixelType, bytes);
-		// Generates MipMaps
 		glGenerateMipmap(texType);
 
-		// Deletes the image data as it is already in the OpenGL Texture object
 		stbi_image_free(bytes);
 
-		// Unbinds the OpenGL Texture object so that it can't accidentally be modified
 		glBindTexture(texType, 0);
 	}
 
 	void texUnit(Shader& shader, const char* uniform, GLuint unit) {
-		// Gets the location of the uniform
 		GLuint texUni = glGetUniformLocation(shader.ID, uniform);
-		// Shader needs to be activated before changing the value of a uniform
 		shader.Activate();
-		// Sets the value of the uniform
 		glUniform1i(texUni, unit);
 	}
 
